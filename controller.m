030205293721a3eae57d8908bcaf7e0672db0aad
Allen Spain <avs81684@uga.edu>
2020-09-16 20:01:39 -0400

    working on function outline

---
+++ controller.m
@@ -0,0 +1,7 @@
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% Spring mass damper system
+% Allen Spain
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+m = 1 % mass (kg)
+k = 0.2  % spring constant 
+b = 
---
+++ rootlocus.m
@@ -0,0 +1,12 @@
+% root locus analysis
+
+% controller root locus
+sys1 = tf([0 0 1],[1 0.01 0.2]);
+rlocus(sys1)
+
+% no energy system (x0,v0) = (0,0)
+sys1 = tf([0 0 1],[1 0.01 0.2]);
+rlocus(sys1)
+
+% save matlab figure
+savefig('./figs/no_energy.png')
