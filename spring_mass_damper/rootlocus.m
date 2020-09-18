259e33949c58b0d738f71216bc218a5cb760a1c7
Allen Spain <aspain2394@gmail.com>
2020-09-18 19:58:06 -0400

    Update rootlocus.m

--- spring_mass_damper/rootlocus.m
+++ spring_mass_damper/rootlocus.m
@@ -1,11 +1,8 @@
-% root locus analysis
+% spring-mass-damper root locus from transfer function sys
 % controller root locus
-sys1 = tf(1,[1 0.01 0.2]);
-rlocus(sys1)
 
-% no energy system (x0,v0) = (0,0)
-sys1 = tf([0 0 1],[1 0.01 0.2]);
-
-% save matlab figure
-% savefig('./figs/ideal_step_response.png')
+% no energy system @ (x0,v0) = (0,0)
+sys = tf(1,[1 0.01 0.2]);
+rlocus(sys)
+savefig('./figs/ideal_step_response.png')
 
