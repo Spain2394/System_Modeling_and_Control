b0cb425706a7e8f37dbcb547df1089ba52e385af
Allen Spain <aspain2394@gmail.com>
2020-09-18 19:59:54 -0400

    Update step_response.m

--- spring_mass_damper/step_response.m
+++ spring_mass_damper/step_response.m
@@ -1,17 +1,4 @@
-% root locus analysis
-
-% controller root locus
-% sys1 = tf(1,[1 0.01 0.2]);
-% % rlocus(sys1)
-% % step response
-% a = step(sys1)
-
-% % % no energy system (x0,v0) = (0,0)
-% % sys1 = tf([0 0 1],[1 0.01 0.2]);
-% % rlocus(sys1)
-
-% % save matlab figure
-% % savefig('./figs/ideal_step_response.png')
+% Take a look at the affect of the damping ratios
 
 m = 1;        % mass (kg)
 k = 0.2;      % spring constant (N/m)
@@ -20,18 +7,10 @@ b =  0.01;    % damper (viscious friction) (Ns/m)
 wn = sqrt(k/m);
 dam = b/(2*sqrt(k*m));
 
-hold on;
+% vary damping 
 for d=0:0.5:1
     sys = tf(1,[1 2*d*wn wn^2]);
     step(sys,100);
-    legend('d' + d)
-    % set(get(gca, 'XLabel', 'String', '');
 end 
+
 saveas(gcf, './figs/damping_step_response.png')    
-
-clf(gcf)
-sys2 = tf(1,[m b k]);
-step(sys2,100);
-
-% xlabel('time(s)');
-saveas(gcf, './figs/current_step_response.png')    
